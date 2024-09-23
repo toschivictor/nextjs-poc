b749fcee3b446ef7b4dc773afdab378668931059
Victor Toschi <victor.toschi@hotmail.com>
2024-09-23 08:31:32 -0300

    Add loading state

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,6 +3,7 @@ interface ButtonProps {
 	backgroundColor?: string;
 	size?: 'small' | 'medium' | 'large';
 	label: string;
+	loading?: boolean;
 	onClick?: () => void;
 }
 
@@ -11,6 +12,7 @@ export const Button = ({
 	size = 'medium',
 	backgroundColor,
 	label,
+	loading = false,
 	...props
 }: ButtonProps) => {
 	const mode = primary ? 'bg-blue-500 text-white' : 'bg-transparent text-gray';
@@ -18,17 +20,16 @@ export const Button = ({
 		<button
 			type="button"
 			className={[
-				'px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4',
+				'flex items-center justify-center px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4',
 				`storybook-button--${size}`,
 				mode,
 			].join(' ')}
 			{...props}
 		>
 			{label}
-			<div
-				className="w-4 h-4 rounded-full animate-spin
-                    border border-solid border-white border-t-transparent"
-			></div>
+			{loading && (
+				<div className="w-4 h-4 rounded-full animate-spin ml-2 border border-solid border-white border-t-transparent" />
+			)}
 			<style jsx>{`
 				button {
 					background-color: ${backgroundColor};
